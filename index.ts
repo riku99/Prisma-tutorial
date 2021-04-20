cff93330e2ea28f9fb07ae465ecb5b6cc922db8a
riku <riku.suny@gmail.com>
2021-04-21 08:13:48 +0900

    add prisma query

--- index.ts
+++ index.ts
@@ -5,9 +5,38 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const main = async () => {
-  // prismaクエリ
-  const allUsers = await prisma.user.findMany();
-  console.log(allUsers);
+  // Userレコードの作成
+  // ネストクエリを使いPostとProfileも作成している
+  await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+      posts: {
+        // Postも作成
+        create: { title: "Hello World" },
+      },
+      profile: {
+        // Profileも作成
+        create: { bio: "I like Prisma" },
+      },
+    },
+  });
+
+  // アップデート処理
+  const post = await prisma.post.update({
+    where: { id: 1 },
+    data: { published: true },
+  });
+
+  console.log(post);
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  });
+  console.dir(allUsers, { depth: null });
 };
 
 main()
